c4451da123b3b05d1f9a300fd6d435183855bdba
Sanjeev Tamang <sanzeevtamang@gmail.com>
2019-06-28 08:29:27 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,10 +7,10 @@ end
 WIN_COMBINATIONS = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,4,6],[2,5,8],[3,4,5],[6,7,8]]
 
 def won?(board)
-  WIN_COMBINATIONS.detect do |sub_array|
-    board[sub_array[0]] == board[sub_array[1]] &&
-    board[sub_array[1]] == board[sub_array[2]] && 
-    position_taken?(board,sub_array[0])
+  WIN_COMBINATIONS.detect do |win_array|
+    board[win_array[0]] == board[win_array[1]] &&
+    board[win_array[1]] == board[win_array[2]] && 
+    position_taken?(board,win_array[0])
   end
 end
 
