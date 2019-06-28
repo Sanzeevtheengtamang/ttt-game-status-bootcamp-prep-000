465bf3f23d76cee9026c9403d3f3d1b53f928078
Sanjeev Tamang <sanzeevtamang@gmail.com>
2019-06-28 08:52:39 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,7 +28,7 @@ end
 
 def winner(board)
   player = won?(board)
-  if board[player.first] == "X" || board[player.first] == "O" || board[player.first] == nil
+  if board[player.first] == "X" || board[player.first] == "O"
     return board[player.first]
   end
 end
